c6b12dc04a1e15623c74252beef5ac17cc485ed7
Supprise0901 <zzsupprise@proton.me>
2024-09-26 16:35:13 +0800

    update modify

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def test_proxy_delay(proxy_name):
         url = f"{clash_api_url}/proxies/{proxy_name}/delay"
         params = {
             "timeout": 5000,  # 5秒超时
-            "url": "http://www.google.com"  # 更换为 Google 的测试 URL
+            "url": "https://www.google.com"  # 更换为 Google 的测试 URL
         }
         try:
             response = requests.get(url, params=params)
@@ -317,11 +317,11 @@ def test_proxy_speed(proxy_name):
     # 不断发起请求直到达到时间限制
     while time.time() - start_time < test_duration:
         response = requests.get(test_url, stream=True, proxies=proxies)
-        for data in response.iter_content(chunk_size=4096):
+        for data in response.iter_content(chunk_size=524288):
             total_length += len(data)
             if time.time() - start_time >= test_duration:
                 break
-        time.sleep(0.1)  # 引入短暂的延迟，防止过快完成
+        time.sleep(0.5)  # 引入短暂的延迟，防止过快完成
 
     # 逐块下载，直到达到 10MB 为止
     # max_size = 10 * 1024 * 1024  # 50MB 转换为字节
